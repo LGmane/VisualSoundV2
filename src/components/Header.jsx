52ba564a764dfa0f603ce0fc545ed7daac40818b
Leon van Gaever <lg109@hdm-stuttgart.de>
2025-01-20 21:09:24 +0100

    Working on Performance

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// src/components/Header.jsx
+export default function Header() {
+    return (
+      <header>
+        <h1>Visual Sound V2</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+      </header>
+    );
+  }
+  
---
+++ src/components/HelloWolrd.jsx
@@ -0,0 +1,5 @@
+// src/components/HelloWorld.jsx
+export default function HelloWorld() {
+    return <h2>Hello, Visual Sound V2!</h2>;
+  }
+  
